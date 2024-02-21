fe1bec913b4ae9d4395f11e54dbff202c10dd0f5
KohanaD <hawaiianwarrior03@gmail.com>
2024-02-20 21:13:47 -0600

    Kohana_branch app trader

---
+++ Script/app_trader.sql
@@ -0,0 +1,46 @@
+--Tables
+SELECT * FROM app_store_apps
+SELECT * FROM play_store_apps
+--Order By install_count for play store
+SELECT name, category, install_count
+FROM play_store_apps
+ORDER BY install_count ASC
+--Game Genre in both stores
+SELECT name
+FROM app_store_apps
+WHERE primary_genre = 'Games'
+
+INTERSECT
+
+SELECT name
+FROM play_store_apps
+WHERE category = 'GAME'
+
+--app store columns
+SELECT name, size_bytes, currency, price, CAST(review_count AS integer) AS review_count, rating, content_rating, primary_genre
+FROM app_store_apps
+WHERE name IN 
+			   (SELECT name 
+			   FROM app_store_apps
+			   WHERE rating >= 4
+			   INTERSECT
+			   SELECT name 
+			   FROM play_store_apps
+			   WHERE rating >=4) 
+ORDER BY rating DESC
+
+--play store columns
+SELECT name, category, rating, CAST(review_count AS integer) AS review_count, size,install_count, type, price, content_rating, genres
+FROM play_store_apps
+WHERE name IN 
+			   (SELECT DISTINCT name 
+			   FROM app_store_apps
+			   WHERE rating >= 4
+			   INTERSECT
+			   SELECT DISTINCT name 
+			   FROM play_store_apps
+			   WHERE rating >=4) 
+ORDER BY rating DESC
+
+
+    
