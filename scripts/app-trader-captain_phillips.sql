76afcc10e9bbb25f7f8b337aad2b887727f85e05
derekbeistad <derekbeistad@gmail.com>
2024-02-22 20:22:44 -0600

    gathered list of top apps to purchase

--- scripts/app-trader-captain_phillips.sql
+++ scripts/app-trader-captain_phillips.sql
@@ -77,7 +77,8 @@ WHERE name IN
 			   WHERE rating >=4)
 ORDER BY review_count DESC
 
--- COMBINING TABLES
+-- COMBINING Both tables TABLES
+-- 		top: name, avg_rating, price
 SELECT DISTINCT a.name,
 		ROUND((a.rating + p.rating) / 2, 2) AS avg_rating,
 		GREATEST(a.price, REPLACE(p.price, '$', '')::NUMERIC) AS highest_price
@@ -86,3 +87,52 @@ INNER JOIN play_store_apps AS p
 ON a.name = p.name
 WHERE ((a.rating + p.rating) / 2) >= 4
 ORDER BY avg_rating DESC;
+
+-- 		top: name, avg_rating, price, life_span
+SELECT DISTINCT a.name,
+		ROUND((a.rating + p.rating) / 2, 2) AS avg_rating,
+		GREATEST(a.price, REPLACE(p.price, '$', '')::NUMERIC) AS highest_price,
+		CASE WHEN ROUND((a.rating + p.rating) / 2, 2) BETWEEN 4.00 AND 4.49 THEN '9 Year Life'
+		ELSE '10 Year Life' END AS life_span
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+ON a.name = p.name
+WHERE ((a.rating + p.rating) / 2) >= 4
+ORDER BY avg_rating DESC;
+
+-- 		top: name, avg_rating, price, life_span, cost, marketing cost
+SELECT sub.name,
+		sub.profit,
+		ROUND(AVG(sub.total_reviews), 0) AS total_reviews_avg
+FROM
+-- 		SUBQUERY
+	(SELECT DISTINCT a.name, /* app name */
+
+					ROUND(ROUND((a.rating + p.rating) / 2 * 2, 0) / 2.0, 1) AS avg_rating, /* average rating between stores rounded to .5 */
+
+					(ROUND(ROUND((a.rating + p.rating) / 2 * 2, 0) / 2.0, 1) * 2) + 1 AS lifespan_in_years, /* lifespan in years. Take AVG rating times 2 and add 1 */
+
+					GREATEST(a.price::MONEY, p.price::MONEY) AS app_price, /* get app price that is the highest between the 2 stores */
+
+						CASE WHEN GREATEST(a.price::MONEY, p.price::MONEY) <= 1::MONEY THEN 10000::MONEY
+						ELSE (GREATEST(a.price::MONEY, p.price::MONEY) * 10000)::MONEY END AS app_cost, /* cost to purchase app. */
+
+					(((ROUND(ROUND((a.rating + p.rating) / 2 * 2, 0) / 2.0, 1) * 2) + 1) * 12000)::MONEY AS  total_marketing_cost, /* marketing cost over lifespan of the app */
+
+					(((ROUND(ROUND((a.rating + p.rating) / 2 * 2, 0) / 2.0, 1) * 2) + 1) * 120000)::MONEY AS total_earnings, /* earnings from adds and in-app purchases */
+
+					((((ROUND(ROUND((a.rating + p.rating) / 2 * 2, 0) / 2.0, 1) * 2) + 1) * 120000)::MONEY) /* profit */ -
+					((((ROUND(ROUND((a.rating + p.rating) / 2 * 2, 0) / 2.0, 1) * 2) + 1) * 12000)::MONEY) /* minus marketing cost */ - 
+					(CASE WHEN GREATEST(a.price::MONEY, p.price::MONEY) <= 1::MONEY THEN 10000::MONEY
+						ELSE (GREATEST(a.price::MONEY, p.price::MONEY) * 10000)::MONEY END) /* minus app_cost */ AS profit, /* profit = earnings - marketing cost - app cost */
+
+						a.review_count::INTEGER + p.review_count as total_reviews
+					
+	FROM app_store_apps AS a
+	INNER JOIN play_store_apps AS p
+	ON a.name = p.name
+	WHERE ((a.rating + p.rating) / 2) >= 4
+	ORDER BY profit DESC, app_price DESC) AS sub
+
+GROUP BY sub.name, sub.profit
+ORDER BY sub.profit DESC, total_reviews_avg DESC;
