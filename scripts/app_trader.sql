20c8e1bbcfb7961f680a4c1ccdbb0b110f23098b
Tim Spratt <timothyspratt88@gmail.com>
2024-02-24 13:48:11 -0600

    new code, cte's

--- scripts/app_trader.sql
+++ scripts/app_trader.sql
@@ -13,6 +13,10 @@ FROM app_store_apps
 
 
 
+
+
+
+
 SELECT name, price, content_rating, rating,
 CAST(app_store_apps.review_count AS integer)
 FROM app_store_apps
@@ -28,6 +32,9 @@ WHERE name IN (SELECT name
 	
 	
 	
+	
+	
+	
 	SELECT name, size_bytes, currency, price, CAST(review_count AS integer) AS review_count, rating, content_rating, primary_genre
 FROM app_store_apps
 WHERE name IN
@@ -39,6 +46,9 @@ WHERE name IN
 			   FROM play_store_apps
 			   WHERE rating >=4)
 ORDER BY review_count DESC
+
+
+
 
 
 
@@ -60,11 +70,18 @@ WHERE a.name IN
 			   
 			   
 			   
+			   
+			   
+			   
 SELECT DISTINCT a.name, ROUND((a.rating + p.rating) / 2, 2) AS avg_rating
 FROM app_store_apps AS a
 INNER JOIN play_store_apps AS p
 ON a.name = p.name
 WHERE ((a.rating + p.rating) / 2) >= 4
+
+
+
+
 
 
 
@@ -87,6 +104,8 @@ ORDER BY avg_rating DESC;
 
 
 
+
+
 SELECT DISTINCT a.name,
 				ROUND(ROUND((a.rating + p.rating) / 2 * 2, 0) / 2.0, 1) AS avg_rating,
 				(ROUND(ROUND((a.rating + p.rating) / 2 * 2, 0) / 2.0, 1) * 2) + 1 AS lifespan_in_years,
@@ -102,6 +121,10 @@ INNER JOIN play_store_apps AS p
 ON a.name = p.name
 WHERE ((a.rating + p.rating) / 2) >= 4
 ORDER BY profit DESC, app_price DESC;
+
+
+
+
 
 
 
@@ -141,3 +164,115 @@ FROM
 
 GROUP BY sub.name, sub.profit
 ORDER BY sub.profit DESC, total_reviews_avg DESC;
+
+
+
+
+
+--CTE code
+
+
+
+
+
+/* NAME, AVERAGE RATING, LIFESPAN, APP PRICE, TOTAL_REVIEWS */
+WITH rating_price AS 
+		(SELECT DISTINCT UPPER(a.name) AS name,
+		 
+				/* average rating between stores rounded to .5 */
+				ROUND(ROUND((a.rating + p.rating) / 2 * 2, 0) / 2.0, 1) AS avg_rating,
+		 
+		 		/* lifespan in years. Take AVG rating times 2 and add 1 */
+		 		(ROUND(ROUND((a.rating + p.rating) / 2 * 2, 0) / 2.0, 1) * 2) + 1 AS lifespan,
+
+				/* get app price that is the highest between the 2 stores */
+				GREATEST(a.price::MONEY, p.price::MONEY) AS app_price,
+		 		
+		 		/* TOTAL REVIEWS */
+		 		a.review_count::INTEGER + p.review_count AS total_reviews
+
+		FROM app_store_apps AS a
+		INNER JOIN play_store_apps AS p
+		ON a.name = p.name
+		WHERE ROUND(ROUND((a.rating + p.rating) / 2 * 2, 0) / 2.0, 1) >= 4.5)
+		
+SELECT  /* NAME */
+		name,
+		
+		/* AVERAGE RATING */
+		avg_rating,
+		
+		/* LIFESPAN IN YEARS */
+		lifespan,
+		
+		/* APP PRICE */
+		app_price,
+		
+		/* APP COST */
+		CASE WHEN app_price <= 1::MONEY THEN 10000::MONEY
+			ELSE (app_price * 10000)::MONEY END AS app_cost,
+			
+		/* MARKETING COST */
+		(lifespan * 12000)::MONEY AS total_marketing_cost,
+		
+		/* EARNINGS */
+		(lifespan * 120000)::MONEY AS total_earnings,
+		
+		/* PROFIT */
+		((lifespan * 120000)::MONEY) /* earnings */ -
+			((lifespan * 12000)::MONEY) /* minus marketing cost */ - 
+			(CASE WHEN app_price <= 1::MONEY THEN 10000::MONEY
+				ELSE (app_price * 10000)::MONEY END) /* minus app_cost */ AS profit,
+		
+		/* TOTAL AMOUNT OF REVIEWS */
+		ROUND(AVG(total_reviews), 0) AS total_reviews
+		
+FROM rating_price
+GROUP BY name, avg_rating, lifespan, app_price
+ORDER BY profit DESC, total_reviews DESC;
+
+
+
+
+--CTE code
+
+
+
+
+
+WITH stores AS
+(SELECT
+	a.name AS app_name,
+	ROUND(ROUND((a.rating + p.rating) / 2 * 2, 0) / 2.0, 1) AS avg_rating,
+	GREATEST(a.price::MONEY, p.price::MONEY) AS app_price,
+	(a.review_count::INTEGER + p.review_count) as total_reviews,
+ 	(ROUND(ROUND((a.rating + p.rating) / 2 * 2, 0) / 2.0, 1)*2)+1 AS lifespan
+	
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+USING (name)
+WHERE ROUND(ROUND((a.rating + p.rating) / 2 * 2, 0) / 2.0, 1) >= 4.5)
+
+
+SELECT
+	app_name,
+	avg_rating,
+	app_price,
+	lifespan,
+	lifespan * 120000::MONEY AS total_revenue,
+	lifespan * 12000::MONEY AS total_marketing_cost,
+	CASE
+		WHEN app_price <= 1::MONEY THEN 10000::MONEY
+		ELSE app_price * 10000 END AS app_cost,
+	(lifespan * 120000::MONEY) -    					--Total Revenue---
+	(lifespan * 12000::MONEY) -     					--Marketing Cost--
+	(CASE												------------------
+		WHEN app_price <= 1::MONEY THEN 10000::MONEY	--App Cost--------
+		ELSE app_price * 10000 END)						------------------
+	AS total_profit,
+	ROUND(AVG(total_reviews),2) AS total_reviews
+	
+FROM stores
+GROUP BY app_name, avg_rating, app_price, lifespan
+ORDER BY total_profit DESC, total_reviews DESC
+LIMIT 10
